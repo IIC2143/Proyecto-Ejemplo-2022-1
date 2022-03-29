d3c44a0fd918c6bbd14569621cfe6d739628d377
Martin Orrego <martinorregosilva@uc.cl>
2022-03-29 00:14:59 -0300

    fix(views): add missing titles and add align

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'publications#index'
+
   get '/profiles', to: 'profiles#index'
   get '/profiles/new', to: 'profiles#new'
   post '/profiles', to: 'profiles#create'
