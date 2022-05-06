fd105af15fdeb3bf3696ea705194c1b9f6cb3ce9
Ignacio Porte <ignacio.porte@uc.cl>
2022-05-06 01:00:08 -0400

    Add Offer Controller and assocations

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,25 @@
+class OffersController < ApplicationController
+    def new
+        @offer = Comment.new
+    end
+
+    def create
+        @offer = Offer.new(offer_params)
+        if @offer.save
+          redirect_to publications_path(@offer.publication)
+        else
+          render :new
+        end
+    end
+
+    def delete
+      @offer = Comment.find(params[:id])
+      @id_publication= @offer.publication
+      @offer.destroy
+      redirect_to publications_path(@id_publication)
+    end
+
+    # def offer_params
+    #     params.require(:offer).permit(:content)
+    # end
+end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Offer < ApplicationRecord
+    belongs_to :offerer, :class_name => "User", optional: true
+    belongs_to :publisher, :class_name => "User", optional: true
 end
