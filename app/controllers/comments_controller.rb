18d973d61ac740cbe7fe0cd4d82d81c976500926
Ignacio Porte <ignacio.porte@uc.cl>
2022-05-06 01:56:15 -0400

    Add comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,21 @@
 class CommentsController < ApplicationController
     def new
+        return redirect_to new_user_session_url if current_user.nil?
+        @publication_id = params[:publication_id]
         @comment = Comment.new
     end
 
     def create
-        @comment = Profile.new(comment_params)
+        return redirect_to new_user_session_url if current_user.nil?
+        comment_params[:publication_id] = params[:publication_id].to_i
+
+        puts ("params: #{comment_params}")  
+        @comment = Comment.new(comment_params)
+        @comment.user = current_user
+        puts ("@comment: #{@comment.inspect}")
         if @comment.save
-          redirect_to publications_path(@comment.publication)
+
+          redirect_to publication_path(@comment.publication_id)
         else
           render :new
         end
@@ -20,6 +29,6 @@ class CommentsController < ApplicationController
     end
 
     def comment_params
-        params.require(:comment).permit(:content)
+        params.require(:comment).permit(:content, :publication_id, :user_id)
     end
 end
