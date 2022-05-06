e22d4b76777f373ab6b88ab2b2c4758def669584
Mari Zambrano <mariela.zambrano@uc.cl>
2022-05-06 17:56:40 -0400

    Agregar validaciones en Publication de models

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,4 +3,6 @@
 class Publication < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
+  validates :title, length: { minimum: 5 }
+  validates :content, length: { minimum: 10 }
 end
