0d1af2a77e6b0ae629c18b080b3863dc4a040f2e
Ignacio Porte <ignacio.porte@uc.cl>
2022-05-06 00:57:54 -0400

    Create Users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# Users Controller
+class UsersController < ApplicationController
+  def profile
+    # redirect to login page if not logged in
+    return redirect_to new_user_session_url if current_user.nil? || current_user.id != params[:id].to_i
+
+    @publications = Publication.where(user_id: current_user.id).all
+    @user = current_user
+    @comments = {}
+    @publications.each do |publication|
+      @comments[publication.id] = publication.comments.order(created_at: :desc)
+    end
+  end
+
+  def delete
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+end
+  
