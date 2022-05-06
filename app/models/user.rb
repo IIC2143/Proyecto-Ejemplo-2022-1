d8c9bef0ca8b684d60780eccf5515d0e65550ef6
Ignacio Porte <ignacio.porte@uc.cl>
2022-05-06 01:56:58 -0400

    Add User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,20 @@
 class User < ApplicationRecord
   has_many :publications, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :offers
+  has_many :offerers, :through => :offers, :class_name => "User"
+  has_many :aplicants, :through => :offers, :class_name => "User"
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :name, length: { minimum: 2 }
+  # validates :description, length: { minimum: 5 }
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
+
+  def summary
+    # "#{name} - #{age} - #{work}"
+  end
 end
